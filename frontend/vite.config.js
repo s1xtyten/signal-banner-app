99fe36d9de9ca30ed18c02b8f9c52c68ed32d261
s1xtyten <102329248+s1xtyten@users.noreply.github.com>
2025-03-27 18:56:22 +0100

    gh pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
     plugins: [svelte()],
+    base: './',
     server: {
         host: true,  // Changed from '127.0.0.1' to true to allow all network access
         port: 5173,
